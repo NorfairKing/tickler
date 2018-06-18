d98bb0dc7fab8dde818c45117e5e099adff684f5
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-18 17:50:07 +0200

    got rid of pandoc

--- default.nix
+++ default.nix
@@ -1,14 +1,10 @@
 let
   pkgsv = import (import ./nixpkgs.nix);
   pkgs = pkgsv {};
-  intray-overlay =
-            import (pkgs.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "intray";
-              rev = "c2e9cfa69553249f165b264dbb288e399d46509c";
-              sha256 = "18zb1w09l6sy35lsknkx45zdlq397qyaqv56y6mww9l9dlspa4w4";
-              fetchSubmodules = true;
-            } + "/overlay.nix");
+  intray-overlay = import (
+    (pkgs.fetchFromGitHub (import ./intray-version.nix) 
+    + "/overlay.nix")
+  );
 in pkgsv {
   overlays = [ intray-overlay (import ./overlay.nix) ];
   config.allowUnfree = true;
---
+++ intray-version.nix
@@ -0,0 +1,7 @@
+{
+  owner = "NorfairKing";
+  repo = "intray";
+  rev = "c2e9cfa69553249f165b264dbb288e399d46509c";
+  sha256 = "18zb1w09l6sy35lsknkx45zdlq397qyaqv56y6mww9l9dlspa4w4";
+  fetchSubmodules = true;
+}
