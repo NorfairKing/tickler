70258ea66f9ef9ec87135c75b583a6c1955edeff
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-25 05:56:35 +0200

    Started on yearly recurrence

--- tickler-data/src/Tickler/Data/Recurrence.hs
+++ tickler-data/src/Tickler/Data/Recurrence.hs
@@ -21,6 +21,10 @@ data Recurrence
     | EveryMonthsOnDay Word
                        (Maybe Word8)
                        (Maybe TimeOfDay)
+    | EveryYearInMonthOnDay Word
+                            (Maybe Word8)
+                            (Maybe Word8)
+                            (Maybe TimeOfDay)
     deriving (Show, Eq, Ord, Generic)
 
 instance Validity Recurrence where
@@ -38,6 +42,23 @@ instance Validity Recurrence where
             , delve "Maybe Word8" md
             , delve "Maybe TimeOfDay" mtod
             , declare "The number of months is strictly positive" $ ms >= 1
+            , declare "The day of the month is strictly positive" $
+              maybe True (>= 1) md
+            , declare "The day of the month is less than or equal to 31" $
+              maybe True (<= 31) md
+            ]
+    validate (EveryYearInMonthOnDay ys mm md mtod) =
+        decorate "EveryYearInMonthOnDay" $
+        mconcat
+            [ delve "Word" ys
+            , delve "Maybe Word8" mm
+            , delve "Maybe Word8" md
+            , delve "Maybe TimeOfDay" mtod
+            , declare "The number of years is strictly positive" $ ys >= 1
+            , declare "The month of the year is strictly positive" $
+              maybe True (>= 1) mm
+            , declare "The month of the year is less than or equal to 12" $
+              maybe True (<= 12) mm
             , declare "The day of the month is strictly positive" $
               maybe True (>= 1) md
             , declare "The day of the month is less than or equal to 31" $
@@ -65,6 +86,18 @@ instance FromJSON Recurrence where
                      (\o' ->
                           EveryMonthsOnDay <$> o' .: "months" <*> o' .:? "day" <*>
                           o' .:? "time-of-day"))
+            v <|>
+        withObject
+            "Recurrence"
+            (\o ->
+                 o .: "every-x-years" >>=
+                 withObject
+                     "EveryYearInMonthOnDay"
+                     (\o' ->
+                          EveryYearInMonthOnDay <$> o' .: "years" <*>
+                          o' .:? "month" <*>
+                          o' .:? "day" <*>
+                          o' .:? "time-of-day"))
             v
 
 instance ToJSON Recurrence where
@@ -74,6 +107,16 @@ instance ToJSON Recurrence where
         object
             [ "every-x-months" .=
               object ["months" .= ms, "day" .= md, "time-of-day" .= mtod]
+            ]
+    toJSON (EveryYearInMonthOnDay ys mm md mtod) =
+        object
+            [ "every-x-months" .=
+              object
+                  [ "years" .= ys
+                  , "month" .= mm
+                  , "day" .= md
+                  , "time-of-day" .= mtod
+                  ]
             ]
 
 instance PersistField Recurrence where
@@ -92,3 +135,8 @@ everyDaysAtTime ds mtod = constructValid $ EveryDaysAtTime ds mtod
 everyMonthsOnDayAtTime ::
        Word -> Maybe Word8 -> Maybe TimeOfDay -> Maybe Recurrence
 everyMonthsOnDayAtTime ms md mtod = constructValid $ EveryMonthsOnDay ms md mtod
+
+everyYearInMonthOnDay ::
+       Word -> Maybe Word8 -> Maybe Word8 -> Maybe TimeOfDay -> Maybe Recurrence
+everyYearInMonthOnDay ys mm md mtod =
+    constructValid $ EveryYearInMonthOnDay ys mm md mtod
