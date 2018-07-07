3532bb8795287a45a5546fe4b5a05f20a0a28713
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-07-07 09:40:20 +0200

    UPgraded intray version

--- intray-version.nix
+++ intray-version.nix
@@ -1,7 +1,7 @@
 {
   owner = "NorfairKing";
   repo = "intray";
-  rev = "4a9b72be4daa701d32d017482f6743a7a5116a41";
-  sha256 = "17xdqkw4hajxjjmd7bygg6h6n2a9xynrc3p4nwg1a6ns05lzglyc";
+  rev = "39fb3f4b165229f63a537a0ffa413e9d81b34322";
+  sha256 = "15v067g5czqbcxa7yadvqa3zjrrsldpqldfwbajfa014hwyyrjqd";
   fetchSubmodules = true;
 }
