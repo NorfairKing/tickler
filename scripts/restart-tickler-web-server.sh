b2e8dd350c4a7a6b116afff42d1f2348ec53cf6d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-03-12 12:21:58 +0100

    front matter for tickler

--- scripts/restart-tickler-web-server.sh
+++ scripts/restart-tickler-web-server.sh
@@ -11,5 +11,6 @@ export TICKLER_WEB_SERVER_PORT=8002
 export TICKLER_WEB_SERVER_DEFAULT_INTRAY_URL="https://api.intray.cs-syd.eu"
 export TICKLER_SERVER_PORT=8003
 export TICKLER_SERVER_WEB_HOST=localhost:${TICKLER_WEB_SERVER_PORT}
+export TICKLER_SERVER_LOOPERS_DEFAULT_ENABLED=False
 
 tickler-web-server serve --persist-logins --admin admin $@ &
