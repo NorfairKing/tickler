322d776e34acaf24b5fc2a00f24a87d74c64381f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-03-31 23:23:11 +0200

    little refactor of tickler-web-server

--- tickler-web-server/src/Tickler/Web/Server/Application.hs
+++ tickler-web-server/src/Tickler/Web/Server/Application.hs
@@ -9,22 +9,6 @@ import Yesod
 import Yesod.Auth
 
 import Tickler.Web.Server.Foundation
-import Tickler.Web.Server.Handler.APIDocs
-import Tickler.Web.Server.Handler.Account
-import Tickler.Web.Server.Handler.Add
-import Tickler.Web.Server.Handler.Admin
-import Tickler.Web.Server.Handler.Checkout
-import Tickler.Web.Server.Handler.Delete
-import Tickler.Web.Server.Handler.DeleteTriggereds
-import Tickler.Web.Server.Handler.Edit
-import Tickler.Web.Server.Handler.EmailTriggerResend
-import Tickler.Web.Server.Handler.EmailTriggerVerify
-import Tickler.Web.Server.Handler.Error
-import Tickler.Web.Server.Handler.Home
-import Tickler.Web.Server.Handler.Loopers
-import Tickler.Web.Server.Handler.RetryTriggered
-import Tickler.Web.Server.Handler.Tickles
-import Tickler.Web.Server.Handler.Triggered
-import Tickler.Web.Server.Handler.Triggers
+import Tickler.Web.Server.Handler
 
 mkYesodDispatch "App" resourcesApp
