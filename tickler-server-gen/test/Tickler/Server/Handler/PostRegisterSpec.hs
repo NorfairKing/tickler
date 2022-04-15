54aa7406679b999bbe6d3d63bbf86ea9f3537ed6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-15 16:42:16 +0200

    login tests in the web server

--- tickler-server-gen/test/Tickler/Server/Handler/PostRegisterSpec.hs
+++ tickler-server-gen/test/Tickler/Server/Handler/PostRegisterSpec.hs
@@ -16,12 +16,27 @@ import Tickler.Server.TestUtils
 spec :: Spec
 spec = withTicklerServer $
   describe "Register" $ do
-    describe "make user" $
-      it "does not crash" $ \cenv ->
+    it "does not crash" $ \cenv ->
+      forAllValid $ \registration -> do
+        nameOrError <- runClient cenv $ clientPostRegister registration
+        case nameOrError of
+          Right NoContent -> pure ()
+          Left err ->
+            let snf = expectationFailure $ "Should not fail with error: " <> show err
+             in case err of
+                  FailureResponse _ resp ->
+                    if HTTP.statusCode (responseStatusCode resp) == 409
+                      then pure ()
+                      else snf
+                  _ -> snf
+    it "returns err409 when the username already exists" $ \cenv ->
+      forAllValid $ \password ->
         forAllValid $ \registration -> do
-          nameOrError <- runClient cenv $ clientPostRegister registration
+          void $ runClient cenv $ clientPostRegister registration
+          nameOrError <-
+            runClient cenv . clientPostRegister $
+              Registration (registrationUsername registration) password
           case nameOrError of
-            Right NoContent -> pure ()
             Left err ->
               let snf = expectationFailure $ "Should not fail with error: " <> show err
                in case err of
@@ -30,21 +45,4 @@ spec = withTicklerServer $
                         then pure ()
                         else snf
                     _ -> snf
-    describe "duplicated users" $
-      it "returns err409 when the username already exists" $ \cenv ->
-        forAllValid $ \password ->
-          forAllValid $ \registration -> do
-            void $ runClient cenv $ clientPostRegister registration
-            nameOrError <-
-              runClient cenv . clientPostRegister $
-                Registration (registrationUsername registration) password
-            case nameOrError of
-              Left err ->
-                let snf = expectationFailure $ "Should not fail with error: " <> show err
-                 in case err of
-                      FailureResponse _ resp ->
-                        if HTTP.statusCode (responseStatusCode resp) == 409
-                          then pure ()
-                          else snf
-                      _ -> snf
-              Right _ -> expectationFailure "Should not have succeeded."
+            Right _ -> expectationFailure "Should not have succeeded."
--- tickler-web-server/test/Tickler/Web/Server/Handler/AuthRSpec.hs
+++ tickler-web-server/test/Tickler/Web/Server/Handler/AuthRSpec.hs
@@ -8,43 +8,75 @@ import TestImport
 import Tickler.Data (parseUsername)
 import Tickler.Web.Server.Foundation
 import Tickler.Web.Server.TestUtils
+import Yesod.Auth
 
 {-# ANN module ("HLint: ignore Reduce duplication" :: String) #-}
 
 spec :: Spec
-spec =
-  ticklerWebServerSpec $
-    ydescribe "RegisterR" $
-      do
-        yit "gets a 200" $ do
-          get $ AuthR registerR
-          statusIs 200
-        yit "registers an example account correctly" $ do
-          registerFlow "example" "example"
-          statusIs 303
-          loc <- getLocation
-          void followRedirect
-          liftIO $ loc `shouldBe` Right AddR
-          statusIs 200
-        yit "fails to register and shows an error if an account with the same username exists" $ do
-          registerFlow "example" "example1"
-          registerFlow "example" "example2"
-          statusIs 303
-          loc <- getLocation
-          liftIO $ loc `shouldBe` Right (AuthR registerR)
-          void followRedirect
-          statusIs 200
-          bodyContains "exists"
-        yit "fails to register and shows an error if the username is not valid" $ do
-          let un = "example with a space"
-          liftIO $ parseUsername un `shouldBe` Nothing
-          registerFlow un "example"
-          statusIs 303
-          loc <- getLocation
-          liftIO $ loc `shouldBe` Right (AuthR registerR)
-          void followRedirect
-          statusIs 200
-          bodyContains "Invalid"
+spec = ticklerWebServerSpec $ do
+  describe "RegisterR" $ do
+    it "GETs a 200" $ do
+      get $ AuthR registerR
+      statusIs 200
+
+    it "registers an example account correctly" $ do
+      registerFlow "example" "example"
+      statusIs 303
+      loc <- getLocation
+      void followRedirect
+      liftIO $ loc `shouldBe` Right AddR
+      statusIs 200
+
+    it "fails to register and shows an error if an account with the same username exists" $ do
+      registerFlow "example" "example1"
+      registerFlow "example" "example2"
+      statusIs 303
+      loc <- getLocation
+      liftIO $ loc `shouldBe` Right (AuthR registerR)
+      void followRedirect
+      statusIs 200
+      bodyContains "exists"
+
+    it "fails to register and shows an error if the username is not valid" $ do
+      let un = "example with a space"
+      liftIO $ parseUsername un `shouldBe` Nothing
+      registerFlow un "example"
+      statusIs 303
+      loc <- getLocation
+      liftIO $ loc `shouldBe` Right (AuthR registerR)
+      void followRedirect
+      statusIs 200
+      bodyContains "Invalid"
+
+  describe "LoginRR" $ do
+    it "GETs a 200" $ do
+      get $ AuthR LoginR
+      statusIs 200
+
+    it "fails when the account does not exist" $ do
+      get $ AuthR LoginR
+      statusIs 200
+      request $ do
+        setMethod methodPost
+        setUrl $ AuthR loginFormPostTargetR
+        addTokenFromCookie
+        addPostParam "userkey" "example"
+        addPostParam "passphrase" "example"
+      statusIs 303
+      locationShouldBe $ AuthR LoginR
+
+    it "fails when the password is wrong" $ do
+      registerFlow "example" "password"
+      get $ AuthR LoginR
+      statusIs 200
+      request $ do
+        setMethod methodPost
+        setUrl $ AuthR loginFormPostTargetR
+        addTokenFromCookie
+        addPostParam "userkey" "example"
+        addPostParam "passphrase" "wrong-password"
+      statusIs 303
+      locationShouldBe $ AuthR LoginR
 
 registerFlow :: Text -> Text -> YesodExample App ()
 registerFlow exampleUsername examplePassphrase = do
