2b52db3102ca40b904e81450f0d7349a3e127427
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-15 13:58:02 +0200

    test for listing tickles when there are some

--- tickler-web-server/test/Tickler/Web/Server/Handler/TicklesSpec.hs
+++ tickler-web-server/test/Tickler/Web/Server/Handler/TicklesSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Tickler.Web.Server.Handler.TicklesSpec where
 
 import Test.Syd.Yesod
@@ -8,9 +10,29 @@ import Tickler.Web.Server.TestUtils
 spec :: Spec
 spec =
   ticklerWebServerSpec $
-    ydescribe "Tickles" $
-      yit "gets a 200 for a logged-in user" $
-        withExampleAccountAndLogin_ $
-          do
-            get TicklesR
-            statusIs 200
+    describe "Tickles" $ do
+      it "gets a 200 for a logged-in user" $
+        withExampleAccountAndLogin_ $ do
+          get TicklesR
+          statusIs 200
+      it "gets a 200 for a logged-in user when there is a tickle" $
+        withExampleAccountAndLogin_ $ do
+          get AddR
+          statusIs 200
+          request $ do
+            setMethod methodPost
+            setUrl AddR
+            addTokenFromCookie
+            addPostParam "contents" "hello"
+            addPostParam "scheduled-day" "2200-12-03"
+            addPostParam "scheduled-time" ""
+            addPostParam "recurrence" "NoRecurrence"
+            addPostParam "days" ""
+            addPostParam "day-time-of-day" ""
+            addPostParam "months" ""
+            addPostParam "day" ""
+            addPostParam "month-time-of-day" ""
+          statusIs 303
+          locationShouldBe AddR
+          _ <- followRedirect
+          statusIs 200
