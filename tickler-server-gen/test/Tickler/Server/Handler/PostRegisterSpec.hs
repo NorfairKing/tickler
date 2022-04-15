727e1919621da83dc0bed21d9154d44858891dbe
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-15 14:47:00 +0200

    dummy test suite for coverage

---
+++ tickler-server-gen/test/Tickler/Server/Handler/PostRegisterSpec.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Tickler.Server.Handler.PostRegisterSpec (spec) where
+
+import Network.HTTP.Types as HTTP
+import Servant.API
+import Servant.Client
+import TestImport
+import Tickler.API
+import Tickler.API.Gen ()
+import Tickler.Client
+import Tickler.Server.TestUtils
+
+spec :: Spec
+spec = withTicklerServer $
+  describe "Register" $ do
+    describe "make user" $
+      it "does not crash" $ \cenv ->
+        forAllValid $ \registration -> do
+          nameOrError <- runClient cenv $ clientPostRegister registration
+          case nameOrError of
+            Right NoContent -> pure ()
+            Left err ->
+              let snf = expectationFailure $ "Should not fail with error: " <> show err
+               in case err of
+                    FailureResponse _ resp ->
+                      if HTTP.statusCode (responseStatusCode resp) == 409
+                        then pure ()
+                        else snf
+                    _ -> snf
+    describe "duplicated users" $
+      it "returns err409 when the username already exists" $ \cenv ->
+        forAllValid $ \password ->
+          forAllValid $ \registration -> do
+            void $ runClient cenv $ clientPostRegister registration
+            nameOrError <-
+              runClient cenv . clientPostRegister $
+                Registration (registrationUsername registration) password
+            case nameOrError of
+              Left err ->
+                let snf = expectationFailure $ "Should not fail with error: " <> show err
+                 in case err of
+                      FailureResponse _ resp ->
+                        if HTTP.statusCode (responseStatusCode resp) == 409
+                          then pure ()
+                          else snf
+                      _ -> snf
+              Right _ -> expectationFailure "Should not have succeeded."
