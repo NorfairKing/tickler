484e684e57286c8430b0e1985be7f4b433c25427
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-21 18:23:44 +0200

    got the add tests to pass

--- tickler-web-server-webdriver/src/Tickler/Web/Server/Webdriver/Add/TestUtils.hs
+++ tickler-web-server-webdriver/src/Tickler/Web/Server/Webdriver/Add/TestUtils.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Tickler.Web.Server.Webdriver.Add.TestUtils where
 
+import Control.Monad
+import qualified Data.Text as T
+import Data.Time
 import Test.Syd
 import Test.Syd.Webdriver
 import Test.Syd.Webdriver.Yesod
@@ -13,7 +17,30 @@ import Tickler.Web.Server.Foundation
 driveAddTickle :: Tickle -> WebdriverTestM App ItemUUID
 driveAddTickle Tickle {..} = do
   findElem (ById "nav-add") >>= click
+
   findElem (ByName "contents") >>= sendKeys tickleContent
+  findElem (ByName "scheduled-day")
+    >>= sendKeys (T.pack (formatTime defaultTimeLocale "%m%d%Y" tickleScheduledDay))
+  forM_ tickleScheduledTime $ \scheduledTime ->
+    findElem (ByName "scheduled-time")
+      >>= sendKeys (T.pack (formatTime defaultTimeLocale "%I%M%p" scheduledTime))
+  forM_ tickleRecurrence $ \case
+    EveryDaysAtTime ds mtod -> do
+      findElem (ById "EveryDay") >>= click
+      findElem (ByName "days") >>= sendKeys (T.pack (show ds))
+      forM_ mtod $ \tod ->
+        findElem (ByName "day-time-of-day")
+          >>= sendKeys (T.pack (formatTime defaultTimeLocale "%I%M%p" tod))
+    EveryMonthsOnDay ms md mtod -> do
+      findElem (ById "EveryMonth") >>= click
+      findElem (ByName "months") >>= sendKeys (T.pack (show ms))
+      forM_ md $ \d ->
+        findElem (ByName "day")
+          >>= sendKeys (T.pack (show d))
+      forM_ mtod $ \tod ->
+        findElem (ByName "month-time-of-day")
+          >>= sendKeys (T.pack (formatTime defaultTimeLocale "%I%M%p" tod))
+
   findElem (ById "submit") >>= submit
   route <- getCurrentRoute
   liftIO $ case route of
