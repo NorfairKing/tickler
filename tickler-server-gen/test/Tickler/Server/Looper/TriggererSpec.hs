d59bfea9bd7219bb976882f59744af60a822a1e7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-02 17:49:57 +0200

    little reformat

--- tickler-server-gen/test/Tickler/Server/Looper/TriggererSpec.hs
+++ tickler-server-gen/test/Tickler/Server/Looper/TriggererSpec.hs
@@ -1,4 +1,4 @@
-module Tickler.Server.Looper.TriggererSpec where
+module Tickler.Server.Looper.TriggererSpec (spec) where
 
 import Data.Time
 import TestImport
@@ -9,21 +9,25 @@ import Tickler.Server.Looper.Triggerer
 spec :: Spec
 spec = do
   describe "shouldBeTriggered" $ it "doesn't crash" $ producesValid3 shouldBeTriggered
+
   describe "ticklerItemLocalScheduledTime" $
     it "makes valid local times" $
       producesValid ticklerItemLocalScheduledTime
+
   describe "makeTriggeredItem" $
     it "produces valid ticklerItems when it succeeds" $
       producesValid3 makeTriggeredItem
+
   describe "nextScheduledTime" $ do
     it "produces valid utc times" $ producesValid3 nextScheduledTime
     it "never produces the same scheduled time and day" $
-      forAllValid $
-        \d ->
-          forAllValid $ \mt -> forAllValid $ \r -> nextScheduledTime d mt r `shouldNotBe` (d, mt)
+      forAllValid $ \d ->
+        forAllValid $ \mt -> forAllValid $ \r -> nextScheduledTime d mt r `shouldNotBe` (d, mt)
+
     it "works for this simple example" $
       nextScheduledTime (fromGregorian 1970 01 01) (Just midday) (EveryDaysAtTime 5 (Just midnight))
         `shouldBe` (fromGregorian 1970 01 06, Just midnight)
+
     it "works for this simple example" $
       nextScheduledTime
         (fromGregorian 1970 01 01)
