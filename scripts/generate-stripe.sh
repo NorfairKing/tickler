71c6aec471d095fe2357e135bed90a92619de393
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-26 18:35:12 +0200

    new intray

--- scripts/generate-stripe.sh
+++ scripts/generate-stripe.sh
@@ -1,6 +1,6 @@
+sudo rm -rf intray-stripe-client
 sudo rm -rf tickler-stripe-client
-sudo rm -rf stripe-client
+cp -rHL $(nix-build nix/pkgs.nix -A generatedIntrayStripeCode) intray-stripe-client
 cp -rHL $(nix-build nix/pkgs.nix -A generatedTicklerStripeCode) tickler-stripe-client
-cp -rHL $(nix-build nix/pkgs.nix -A generatedStripeCode) stripe-client
+sudo chmod -R +rwx intray-stripe-client
 sudo chmod -R +rwx tickler-stripe-client
-sudo chmod -R +rwx stripe-client
